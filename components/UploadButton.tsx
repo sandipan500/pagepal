f806c7ceb2839849a5aa90fcf68168563c028e40
Sandy <sandipanb223@gmail.com>
2023-10-13 12:11:15 +0530

    first commit

---
+++ components/UploadButton.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
+import { Button } from './ui/button';
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v)
+        }
+      }}
+    >
+      <DialogTrigger
+        onClick={() => setIsOpen(true)}
+        asChild
+      >
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        Example Content
+      </DialogContent>
+    </Dialog>
+  )
+};
+
+export default UploadButton;
