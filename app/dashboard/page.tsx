7c2f19382ea198d90be75d14a73fc9300fa46b38
Sandy <sandipanb223@gmail.com>
2023-10-13 18:02:24 +0530

    third commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
 
-const Dashboard = () => {
-  const {getUser} = getKindeServerSession();
+import { db } from '@/db';
+import Dashboard from '@/components/Dashboard';
+
+const Page = async () => {
+  const { getUser } = getKindeServerSession();
   const user = getUser();
 
+  if (!user || !user.id) redirect ('/auth-callback?origin=dashboard');
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
+
+  if (!dbUser) redirect ('/auth-callback?origin=dashboard')
+
   return (
-    <div>Dashboard</div>
+    <Dashboard />
   )
 };
 
-export default Dashboard;
+export default Page;
