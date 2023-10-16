d7e7e63dfab33a7e43412b1c9a3c48334ea7c8f6
Sandy <sandipanb223@gmail.com>
2023-10-16 13:33:10 +0530

    fourth commit

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -2,14 +2,47 @@
 
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
-import { Cloud } from 'lucide-react';
+import { Cloud, File } from 'lucide-react';
 
 import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 import { Button } from './ui/button';
+import { Progress } from './ui/progress';
 
 const UploadDropzone = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval)
+          return prevProgress
+        }
+        return prevProgress + 5
+      })
+    }, 500)
+
+    return interval
+  };
+
   return (
-    <Dropzone multiple={false}>
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => {
+        setIsUploading(true)
+
+        const progressInterval = startSimulatedProgress()
+
+        //handle file uploading
+
+        clearInterval(progressInterval)
+        setUploadProgress(100)
+      }}
+    >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
           {...getRootProps()}
@@ -18,9 +51,9 @@ const UploadDropzone = () => {
           <div className='flex w-full h-full items-center justify-center'>
             <label
               htmlFor='dropzone-file'
-              className=''
+              className='flex flex-col w-full h-full items-center justify-center rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'
             >
-              <div className='flex flex-col w-full h-full items-center justify-center rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
+              <div className='flex flex-col pt-5 pb-6 items-center justify-center'>
                 <Cloud className='h-6 w-6 text-zinc-500 mb-2' />
                 <p className='mb-2 text-sm text-zinc-700'>
                   <span className='font-semibold'>
@@ -32,6 +65,26 @@ const UploadDropzone = () => {
                   PDF (up to 4MB)
                 </p>
               </div>
+
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className='flex bg-white max-w-xs rounded-md items-center overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200'>
+                  <div className='px-3 py-2 h-full grid place-items-center'>
+                    <File className='h-4 w-4 text-violet-500' />
+                  </div>
+                  <div className='px-3 py-2 text-sm truncate'>
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              {isUploading ? (
+                <div className='w-full mt-4 max-w-xs mx-auto'>
+                  <Progress
+                    className='h-1 w-full bg-zinc-200'
+                    value={uploadProgress}
+                  />
+                </div>
+              ) : null}
             </label>
           </div>
         </div>
