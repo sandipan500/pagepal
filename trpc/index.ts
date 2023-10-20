6e2c44efec1a2722876c177328a66750855613d6
Sandy <sandipanb223@gmail.com>
2023-10-20 16:16:31 +0530

    sixth commit

--- trpc/index.ts
+++ trpc/index.ts
@@ -76,6 +76,20 @@ export const appRouter = router({
     })
 
     return file
+  }),
+  getFileUploadStatus: privateProcedure.input(
+    z.object({ fileId: z.string() })
+  ).query(async ({ ctx, input }) => {
+    const file = await db.file.findFirst({
+      where: {
+        id: input.fileId,
+        userId: ctx.userId
+      }
+    })
+
+    if (!file) return { status: 'PENDING' as const }
+
+    return { status: file.uploadStatus }
   })
 });
 
