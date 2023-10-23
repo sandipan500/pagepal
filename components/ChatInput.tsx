b613d80e9b4b9a200b87cadae443920b59cb4f56
Sandy <sandipanb223@gmail.com>
2023-10-23 22:40:45 +0530

    second commit

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import { Send } from 'lucide-react';
+
 import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
-import { Send } from 'lucide-react';
+import { ChatContext } from '@/context/ChatContext';
 
 interface ChatInputProps {
   isDisabled?: boolean;
 };
 
 const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  const {addMessage, handleInputChange, message, isLoading} = useContext(ChatContext);
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
   return (
     <div className='absolute bottom-0 left-0 w-full'>
       <form className='flex flex-grow mx-2 gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
@@ -19,12 +25,31 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 rows={1}
                 maxRows={4}
                 className='resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'
+                ref={textareaRef}
+                onChange={handleInputChange}
+                value={message}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault()
+
+                    addMessage()
+
+                    textareaRef.current?.focus()
+                  }
+                }}
                 placeholder='Enter your question...'
               />
 
               <Button
+                type='submit'
+                onClick={() => {
+                  addMessage()
+
+                  textareaRef.current?.focus()
+                }}
                 className='absolute bottom-1.5 right-[8px]'
                 aria-label='send message'
+                disabled={isLoading || isDisabled}
               >
                 <Send className='w-4 h-4' />
               </Button>
