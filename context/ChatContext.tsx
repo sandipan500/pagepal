ff5031de50295810c8bfdfbc6591be67393bdd65
Sandy <sandipanb223@gmail.com>
2023-10-23 22:43:06 +0530

    second commit

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -3,6 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import { trpc } from '@/app/_trpc/client';
 import { useToast } from '@/components/ui/use-toast';
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
 
 type StreamResponse = {
   addMessage: () => void
@@ -52,6 +53,145 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
 
       return response.body
     },
+    onMutate: async ({ message }) => {
+      backupMessage.current = message
+      setMessage('')
+
+      await utils.getFileMessages.cancel()
+
+      const previousMessages = utils.getFileMessages.getInfiniteData()
+
+      utils.getFileMessages.setInfiniteData(
+        {fileId, limit: INFINITE_QUERY_LIMIT},
+        (old) => {
+          if(!old) {
+            return {
+              pages: [],
+              pageParams: []
+            }
+          }
+
+          let newPages = [...old.pages]
+
+          let latestPages = newPages[0]!
+
+          latestPages.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true
+            },
+            ...latestPages.messages
+          ]
+
+          newPages[0] = latestPages
+
+          return {
+            ...old,
+            pages: newPages
+          }
+        }
+      )
+
+      setIsLoadning(true)
+
+      return {
+        previousMessages: previousMessages?.pages.flatMap(
+          (page) => page.messages
+        ) ?? []
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current)
+      utils.getFileMessages.setData(
+        {fileId},
+        {messages: context?.previousMessages ?? []}
+      )
+    },
+    onSuccess: async (stream) => {
+      setIsLoadning(false)
+
+      if(!stream) {
+        return toast({
+          title: 'There was a problem sending this message',
+          description: 'Please refresh this page and try again',
+          variant: 'destructive'
+        })
+      }
+
+      const reader = stream.getReader()
+      const decoder = new TextDecoder()
+      let done = false
+
+      let accResponse = ''
+
+      while(!done) {
+        const {value, done: doneReading} = await reader.read()
+        done = doneReading
+        const chunkValue = decoder.decode(value)
+
+        accResponse += chunkValue
+
+        utils.getFileMessages.setInfiniteData(
+          {fileId, limit: INFINITE_QUERY_LIMIT},
+          (old) => {
+            if (!old) return { pages: [], pageParams: [] }
+
+            let isAIResponseCreated = old.pages.some(
+              (page) => page.messages.some(
+                (message) => message.id === 'ai-response'
+              )
+            )
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages
+
+                if (!isAIResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: 'ai-response',
+                      text: accResponse,
+                      isUserMessage: false
+                    },
+                    ...page.messages
+                  ]
+                } else {
+                  updatedMessages = page.messages.map((message) => {
+                    if (message.id === 'ai-response') {
+                      return {
+                        ...message,
+                        text: accResponse
+                      }
+                    }
+                    return message
+                  })
+                }
+
+                return {
+                  ...page,
+                  messages: updatedMessages
+                }
+              }
+
+              return page
+            })
+
+            return {
+              ...old,
+              pages: updatedPages
+            }
+          }
+        )
+      }
+    },
+    onSettled: async () => {
+      setIsLoadning(false)
+
+      await utils.getFileMessages.invalidate({ fileId })
+    }
   })
 
   const handleInputChange = (
